73656b1d18ed434873e86a4df8303d7db50f8182
Zane Ervin <zaneerv@gmail.com>
2019-12-09 01:34:44 -0800

    updated database and ran migration

--- BlogServer/Migrations/20191209045040_AddUserTable.Designer.cs
+++ BlogServer/Migrations/20191209045040_AddUserTable.Designer.cs
@@ -3,14 +3,16 @@ using BlogServer.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BlogServer.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20191209045040_AddUserTable")]
+    partial class AddUserTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
